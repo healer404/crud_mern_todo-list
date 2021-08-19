4df29cca49957e977f1ee7d96d63fa254e8b0b5a
Healer404 <dukaroniel0701@gmail.com>
2021-08-19 18:51:31 +0800

    Configured the MongoDB Database
    The "db.js" contained the configuration of the project.

---
+++ db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+module.exports = async () => {
+    try{
+        const connectionParams = {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        };
+        await mongoose.connect(
+            "mongodb://localhost/todo-app",
+            connectionParams
+        );
+        console.log(
+            "Connected to the database."
+        );
+    } catch (error){
+        console.log(
+            "Could not connect to the database.",
+            error
+        );
+    }
+};
